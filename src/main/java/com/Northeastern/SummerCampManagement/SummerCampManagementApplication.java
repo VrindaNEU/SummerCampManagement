16328f02f89bfd80e1b9796c1addb31e88161a26
vrindashinde <vrindashinde00@gmail.com>
2024-04-01 12:17:04 -0400

    Repository

--- src/main/java/com/Northeastern/SummerCampManagement/SummerCampManagementApplication.java
+++ src/main/java/com/Northeastern/SummerCampManagement/SummerCampManagementApplication.java
@@ -1,13 +1,19 @@
 package com.Northeastern.SummerCampManagement;
 
+import com.Northeastern.SummerCampManagement.Dao.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SummerCampManagementApplication {
-
+@Autowired
+ActivityRepository activityRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SummerCampManagementApplication.class, args);
+                
+               
 	}
 
 }
